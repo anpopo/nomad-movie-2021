3e8f9e562b8bfae7de4c900db3cd80555e70c9f9
anpopo <dkstpgud@gmail.com>
2021-06-04 09:52:51 +0900

    state에 관해 setState 시 React.Component에서 받아온 render 함수를 다시 실행 한다. (변경이 있는 부분만 변화)

--- src/App.js
+++ src/App.js
@@ -1,65 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+class App extends React.Component {
 
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.QKNL3H-b8pGMgfg77Ig8BAHaE8%26pid%3DApi&f=1",
-    rating: 5.0
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP._sycpB50kOtdSk5VD6QyqQHaEK%26pid%3DApi&f=1",
-    rating: 4.7
-  },
-  {
-    id: 3,
-    name: "샤브샤브",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.X36wkzRWkRYl6F4v2yfpMQHaE8%26pid%3DApi&f=1",
-    rating: 4.9
-  },
-  {
-    id: 4,
-    name: "야채비빔밥",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.HRE2MfhsfuvATQpmmGuY4wHaE8%26pid%3DApi&f=1",
-    rating: 3.8
-  },
-  {
-    id: 5,
-    name: "야채죽",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Zv3gzdZ69o4nCCCJYwDQIgHaEK%26pid%3DApi&f=1",
-    rating: 4.3
-  },
-];
+  state = {
+    count: 0
+  };
 
+  add = () => {
+    this.setState(current => ({count: current.count + 1}))
+  }
 
-function Food({ name, pic, rating }) {
+  minus = () => {
+    this.setState(current => ({count: current.count -1}))
+  }
+  
+  render() {
+    return <div>
+      <h1>the number is : {this.state.count}</h1>
+      
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      
+      
+      
+      </div>
+  }
 
-  return <div>
-      <h2>i like {name}</h2>
-      <h4>rating: {rating}</h4>
-      <img src={pic} alt={name}></img>
-    </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number
-
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish =>
-        <Food key={dish.id} name={dish.name} pic={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
 }
 
 export default App;
