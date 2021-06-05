083974cf7fba03812c7ac349e43b508605ed4758
anpopo <dkstpgud@gmail.com>
2021-06-05 17:37:06 +0900

    movie detail url 변경

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import './Movie.css'
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return (
         <Link to={{
-            pathname: "/movie-detail",
+            pathname: `/movie/${id}`,
             state: {
                 year,
                 title,
@@ -26,11 +26,11 @@ function Movie({ year, title, summary, poster, genres }) {
                         ))}
                     </ul>
                     <p className="movie__summary">{summary.slice(0, 140)}...</p>
-                    
+
                 </div>
             </div>
         </Link>
-        );
+    );
 }
 
 Movie.propTypes = {
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,19 +2,19 @@ import React from "react";
 
 class Detail extends React.Component {
 
-    
-
-
     componentDidMount() {
-        const { location, history} = this.props;
-        if(!location.state) {
+        const { location, history } = this.props;
+        if (!location.state) {
             history.push("/");
         }
     }
 
     render() {
-        const {location} = this.props;
-        return <span>{location.state.title}</span>
+        const { location } = this.props;
+        if (location.state) {
+            return <span>{location.state.title}</span>
+        } else return null;
+
     }
 }
 
